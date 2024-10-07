2cbb2d10ae572f4bfa005f2d30aa6c751c541815
abdullahalbatairy <amb6045@gmail.com>
2024-10-07 21:17:31 +0300

    feat: persist the progress of deleting the media for a user when webhook token is expired

--- src/media.ts
+++ src/media.ts
@@ -1,4 +1,5 @@
 import { Collection, CommandInteraction, DiscordAPIError, Message, SlashCommandBuilder, TextChannel } from 'discord.js'
+import { promises } from 'fs'
 
 export const data = new SlashCommandBuilder()
     .setName('media')
@@ -20,8 +21,19 @@ export async function execute(interaction: CommandInteraction) {
 
 
 
+
+
     if (userId === '352190104540020737') {
         await interaction.deferReply({ ephemeral: true });
+        const file = await promises.readFile(`${userId}-${interaction.channel.id}.txt`, 'utf8').catch(() => {
+            return;
+        });
+        if(file) { 
+            lastId = file.split(',')[0];
+            attachments = Number(file.split(',')[1]);
+            totalProcessed = Number(file.split(',')[2]);
+            deletionErrors = Number(file.split(',')[3]);
+        }
         while (true) {
             try {
                 const messages = await channel.messages.fetch({
@@ -30,7 +42,10 @@ export async function execute(interaction: CommandInteraction) {
                 });
 
 
-                if (messages.size === 0) break;
+                if (messages.size === 0) {
+                    await promises.rm(`./${userId}-${interaction.channel.id}.txt`, { force: true });
+                    break;
+                }
 
                 totalProcessed += messages.size;
 
@@ -70,11 +85,23 @@ export async function execute(interaction: CommandInteraction) {
                 }
 
 
-                if (messages.size < 100) break;
+                if (messages.size < 100) {
+                    await promises.rm(`./${userId}-${interaction.channel.id}.txt`, { force: true });
+                    break;
+                }
 
                 await new Promise(resolve => setTimeout(resolve, 100));
 
             } catch (error) {
+                if(error instanceof DiscordAPIError && error.code === 50027) {
+                    await promises.writeFile(`./${userId}-${interaction.channel.id}.txt`, `${lastId},${attachments},${totalProcessed},${deletionErrors}`);
+                    const percent = totalProcessed/interaction.channel.messages.cache.size*100;
+                    const percentage = `${percent.toFixed(2)}%`
+                    await interaction.user.send(`the token expired while you are trying delete all media in ${interaction.channel.name}! run the command again in the same channel to resume the process.`);
+                    return
+                }
+
+
                 console.error('Error fetching messages:', error);
                 await interaction.editReply('An error occurred while processing messages.');
                 return;
