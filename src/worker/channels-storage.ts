a17a4d54ee66decc9e254da77d29eabc9bcadb92
AbdullahAlbatairy <amb6045@gmail.com>
2024-10-17 20:58:47 +0300

    feat(channels-storage): store only text channels that is only in games or text categories to db

--- src/worker/channels-storage.ts
+++ src/worker/channels-storage.ts
@@ -1,33 +1,76 @@
-import {Guild} from 'discord.js';
+import {Collection, Guild, NonThreadGuildBasedChannel, TextChannel} from 'discord.js';
 import {addChannelMessageTracker, beginTransaction, commit, getAllChannelMessageTrackers, rollback} from "../db/sqlite";
+import {config} from '../config';
 
 
-export async function channelsStorage(guild: Guild | undefined) {
+export let currentTextChannel: TextChannel | null;
+export let currentTrackedChannel: ChannelMessageTracker | null;
+let channelTracker: ChannelMessageTracker[] | undefined;
+let channels: Collection<string, NonThreadGuildBasedChannel | null>;
+
+export async function storeChannels(guild: Guild | undefined) {
     if (!guild) return;
-    const channels = await guild.channels.fetch();
+    channels = await guild.channels.fetch();
 
 
-    const tracker = await getAllChannelMessageTrackers();
+    channelTracker = await getAllChannelMessageTrackers();
 
     try {
         await beginTransaction()
-        channels.forEach(async channel => {
+        for (const channel of channels.values()) {
+            if (!channel?.id || channel?.type !== 0) continue;
+
             let exist = false;
-            if (tracker) {
-                for (const track of tracker) {
-                    if (track.channel_id === channel?.id) {
-                        exist = true;
-                        break;
+            if (channelTracker) {
+                exist = channelTracker.some(track => track.channel_id === channel.id);
+            }
+
+            if (!exist) {
+                if (channel.parent?.id === config.GAME_CATEGORY_ID || channel.parent?.id === config.TEXT_CHANNEL_ID) {
+                    try {
+                        await addChannelMessageTracker(channel.id);
+                        console.log(`Added tracker for channel: ${channel.id}`);
+                    } catch (error) {
+                        console.error(`Error adding tracker for channel ${channel.id}:`, error);
                     }
                 }
             }
-            if (!exist && channel?.id && channel?.type === 0) {
-                await addChannelMessageTracker(channel.id);
-            }
-        })
+        }
+
         await commit()
+
+        if (!channelTracker) channelTracker = await getAllChannelMessageTrackers();
+
+        if (channelTracker) {
+            for (const channel of channelTracker) {
+                if (!channel.is_finished) {
+                    currentTrackedChannel = channel;
+                    currentTextChannel = channels.get(`${channel.channel_id}`) as TextChannel
+                    console.log(currentTrackedChannel)
+                    break;
+                }
+            }
+        }
     } catch (e) {
         await rollback();
+    }
+
+
+}
+
+
+export async function updateChannelTracker() {
+    currentTrackedChannel = null;
+    currentTextChannel = null
+    channelTracker = await getAllChannelMessageTrackers();
+    if (channelTracker) {
+        for (const channel of channelTracker) {
+            if (!channel.is_finished) {
+                currentTrackedChannel = channel;
+                currentTextChannel = channels.get(`${channel.channel_id}`) as TextChannel
+                break;
+            }
+        }
     }
 
 
