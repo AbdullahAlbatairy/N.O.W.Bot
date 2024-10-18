afb82565daf8f456ce2f595734bd16471bd8ff9e
AbdullahAlbatairy <amb6045@gmail.com>
2024-10-18 16:09:15 +0300

    feat(constant): create constant file for all variables that are used more than 3 times

---
+++ src/constant.ts
@@ -0,0 +1,11 @@
+export const discordEmojiRegExp = /<:([\w]+):\d+>/g;
+
+export const tableNames = {
+    messages: "messages",
+    emoji: "emoji",
+    emoji_count: "emoji_count",
+    channel_message_tracker: "channel_message_tracker",
+}
+
+
+
