e9ed346d4248fc864729c081b1fcb5c8500c53d0
AbdullahAlbatairy <amb6045@gmail.com>
2024-11-27 19:31:10 +0300

    introduce new filter emoji commands

--- src/commands/emoji1.ts
+++ src/commands/emoji1.ts
@@ -1,18 +1,18 @@
-
 import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { getEmojisCount } from "../db/sqlite";
 import { createEmojiStatsMessage } from "../message-formatter";
-import { getEmojisCount, getLeastUsedEmoji } from "../db/sqlite";
+import { Period, THIRTY_DAYS_AGO } from "../constant/time-range";
 
 export const data = new SlashCommandBuilder()
-    .setName('emoji')
-    .setDescription('check emoji usage for the server all time');
+    .setName('emoji1')
+    .setDescription('check emoji usage for the server for 30 days');
 
 
 export async function execute(interaction: CommandInteraction) {
     await interaction.deferReply();
     try {
-        const emojiCounts = await getEmojisCount();
-        const messageOptions = createEmojiStatsMessage(emojiCounts);
+        const emojiCounts = await getEmojisCount(THIRTY_DAYS_AGO);
+        const messageOptions = createEmojiStatsMessage(emojiCounts, Period.THIRTY_DAYS_AGO);
         await interaction.editReply(messageOptions);
     } catch (error) {
         console.error('Error fetching emoji stats:', error);
--- src/commands/emoji6.ts
+++ src/commands/emoji6.ts
@@ -1,18 +1,18 @@
-
 import { CommandInteraction, SlashCommandBuilder } from "discord.js";
+import { getEmojisCount } from "../db/sqlite";
 import { createEmojiStatsMessage } from "../message-formatter";
-import { getEmojisCount, getLeastUsedEmoji } from "../db/sqlite";
+import { Period, SIX_MONTHS_AGO } from "../constant/time-range";
 
 export const data = new SlashCommandBuilder()
-    .setName('emoji')
-    .setDescription('check emoji usage for the server all time');
+    .setName('emoji6')
+    .setDescription('check emoji usage for the server for the last 6 months');
 
 
 export async function execute(interaction: CommandInteraction) {
     await interaction.deferReply();
     try {
-        const emojiCounts = await getEmojisCount();
-        const messageOptions = createEmojiStatsMessage(emojiCounts);
+        const emojiCounts = await getEmojisCount(SIX_MONTHS_AGO);
+        const messageOptions = createEmojiStatsMessage(emojiCounts, Period.SIX_MONTHS_AGO);
         await interaction.editReply(messageOptions);
     } catch (error) {
         console.error('Error fetching emoji stats:', error);
